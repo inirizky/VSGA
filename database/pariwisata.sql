c9c37557090c579cab7201ec3fb43a4801ad0ca1
Ini Rizky <85105054+inirizky@users.noreply.github.com>
2024-06-14 02:35:25 +0700

    Database

---
+++ database/pariwisata.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 13, 2024 at 07:33 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pariwisata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_tiket`
+--
+
+CREATE TABLE `tb_tiket` (
+  `id` int NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone_number` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `booking_date` date NOT NULL,
+  `duration` int NOT NULL,
+  `number_of_people` int NOT NULL,
+  `isHotel` tinyint(1) NOT NULL DEFAULT '0',
+  `isTransport` tinyint(1) NOT NULL DEFAULT '0',
+  `isCatering` tinyint(1) NOT NULL DEFAULT '0',
+  `packet_price` double NOT NULL,
+  `total_price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_tiket`
+--
+
+INSERT INTO `tb_tiket` (`id`, `name`, `phone_number`, `booking_date`, `duration`, `number_of_people`, `isHotel`, `isTransport`, `isCatering`, `packet_price`, `total_price`) VALUES
+(2, 'Yanto Kocul', '085155322142', '2024-06-14', 2, 4, 1, 1, 1, 1200000, 9600000),
+(3, 'Yanto Kocul', '085155322142', '2024-06-14', 2, 4, 0, 1, 0, 1200000, 9600000),
+(6, 'Yanto Mantap', '085155322142', '2024-06-14', 2, 4, 0, 1, 0, 1200000, 9600000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_tiket`
+--
+ALTER TABLE `tb_tiket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_tiket`
+--
+ALTER TABLE `tb_tiket`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
